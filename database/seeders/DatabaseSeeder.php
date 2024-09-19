c3dcf47d933bd905fd5da4395b62e34d4b48bbb5
Irwing Naranjo <khalisser@gmail.com>
2024-09-19 17:36:29 -0300

    Modulo Ciudades: archivos de generación de datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Irwing Naranjo',
+            'email' => 'khalisser@gmail.com',
+            'password' => bcrypt('qwerty123'),
+        ]);
+
+        $this->call([
+            CountrySeeder::class,
         ]);
     }
 }
