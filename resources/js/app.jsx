50a0c6945733e92022d117ed29c8b4ee4360a566
Irwing <khalisser@gmail.com>
2024-09-20 01:54:36 -0300

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,21 +1,25 @@
-import './bootstrap'
-import '../css/app.css'
+import ReactDOMServer from 'react-dom/server';
+import { createInertiaApp } from '@inertiajs/react';
+import createServer from '@inertiajs/react/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { route } from '../../vendor/tightenco/ziggy';
 
-import { createRoot } from 'react-dom/client'
-import { createInertiaApp } from '@inertiajs/react'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: ReactDOMServer.renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+        setup: ({ App, props }) => {
+            global.route = (name, params, absolute) =>
+                route(name, params, absolute, {
+                    ...page.props.ziggy,
+                    location: new URL(page.props.ziggy.location),
+                });
 
-createInertiaApp({
-  title: (title) => `${title} - ${appName}`,
-  resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-  setup ({ el, App, props }) {
-    const root = createRoot(el)
-
-    root.render(<App {...props} />)
-  },
-  progress: {
-    color: '#4B5563'
-  }
-})
+            return <App {...props} />;
+        },
+    })
+);
