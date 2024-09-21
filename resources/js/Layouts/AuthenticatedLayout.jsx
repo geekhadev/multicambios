57366e3c9eef932bc71937a14f720202d4e7429f
Irwing Naranjo <khalisser@gmail.com>
2024-09-21 13:56:56 -0300

    Add module and action props to AuthenticatedLayout and update CountriesIndex page

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -2,7 +2,7 @@ import Dropdown from '@/Components/Dropdown'
 import NavLink from '@/Components/NavLink'
 import { Link, usePage } from '@inertiajs/react'
 
-export default function Authenticated ({ header, children }) {
+export default function Authenticated ({ header, children, module, action }) {
   const user = usePage().props.auth.user
 
   return (
@@ -81,29 +81,37 @@ export default function Authenticated ({ header, children }) {
           <nav className="flex" aria-label="Breadcrumb">
             <ol className="inline-flex items-center">
               <li className="inline-flex items-center">
-                <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                <span className="inline-flex items-center text-sm font-medium text-gray-700">
                   <svg className="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
                   </svg>
-                  Home
-                </a>
+                  Multicambios
+                </span>
               </li>
-              <li>
-                <div className="flex items-center">
-                  <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                  </svg>
-                  <a href="#" className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Projects</a>
-                </div>
-              </li>
-              <li aria-current="page">
-                <div className="flex items-center">
-                  <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                  </svg>
-                  <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Flowbite</span>
-                </div>
-              </li>
+              {module && (
+                <li>
+                  <div className="flex items-center">
+                    <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <span className="ms-1 text-sm font-medium text-gray-700">
+                      {module}
+                    </span>
+                  </div>
+                </li>
+              )}
+              {action && (
+                <li aria-current="page">
+                  <div className="flex items-center">
+                    <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2">
+                      {action}
+                    </span>
+                  </div>
+                </li>
+              )}
             </ol>
           </nav>
         </div>
--- resources/js/Pages/Countries/Index.jsx
+++ resources/js/Pages/Countries/Index.jsx
@@ -16,7 +16,7 @@ export default function CountriesIndex ({ countries }) {
   }
 
   return (
-    <AuthenticatedLayout>
+    <AuthenticatedLayout module={'Países'}>
       <Head title="Países" />
 
       <div>
@@ -27,16 +27,7 @@ export default function CountriesIndex ({ countries }) {
                 <h4 className="text-lg font-bold">Países</h4>
               </div>
               <div className='flex gap-4'>
-                <a href={route('dashboard.countries.create')} className="text-gray-500 hover:text-gray-700 flex items-center gap-1">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-file-type-csv"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4" /><path d="M7 16.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0" /><path d="M10 20.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75" /><path d="M16 15l2 6l2 -6" /></svg>
-                  Descargar
-                </a>
-                <a href={route('dashboard.countries.create')}
-                className="
-                  flex items-center gap-1 font-bold
-                  bg-blue-600 text-white px-3 py-1 rounded-full
-                  hover:bg-blue-700 transition duration-200
-                ">
+                <a href={route('dashboard.countries.create')} className="flex items-center gap-1 font-bold bg-blue-600 text-white px-3 py-1 rounded-full hover:bg-blue-700 transition duration-200">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
                   Crear
                 </a>
