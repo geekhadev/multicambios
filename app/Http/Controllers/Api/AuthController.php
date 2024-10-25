3136c0aafce23460b0980660bf8d84de01b918a3
Irwing Naranjo <khalisser@gmail.com>
2024-10-24 22:55:11 -0300

    feat: add template login auth for api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        // implement here...
+    }
+}
