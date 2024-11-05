26abd28c56f0380bbaba5ad97b6c5ad8c69d3c22
Irwing Naranjo <khalisser@gmail.com>
2024-11-04 22:12:20 -0300

    refactor: user add phone field for login by app

--- database/migrations/2024_10_31_214757_create_customers_table.php
+++ database/migrations/2024_10_31_214757_create_customers_table.php
@@ -9,14 +9,22 @@ return new class extends Migration {
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
-            $table->foreignId('country_id');
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('document_type', ['passport', 'id'])->default('id');
+            $table->string('document_number');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
             $table->string('phone');
+            $table->foreignId('country_id')->constrained('countries');
+            $table->foreignId('state_id')->constrained('states');
+            $table->text('address')->nullable();
+            $table->string('occupation')->nullable();
+            $table->boolean('politically_exposed')->default(false);
             $table->boolean('is_confirmed')->default(false);
             $table->boolean('is_active')->default(true);
+            $table->foreignId('confirmed_by')->nullable()->constrained('users');
+            $table->timestamp('confirmed_at')->nullable();
             $table->timestamps();
         });
     }
