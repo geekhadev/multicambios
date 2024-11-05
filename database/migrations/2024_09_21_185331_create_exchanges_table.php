2c6f6a4a80fd5e064b1b6359b515dfb96c889951
Irwing Naranjo <khalisser@gmail.com>
2024-11-05 20:23:27 -0300

    doc: exchange add comments fix migration

--- database/migrations/2024_09_21_185331_create_exchanges_table.php
+++ database/migrations/2024_09_21_185331_create_exchanges_table.php
@@ -20,17 +20,15 @@ return new class extends Migration
             $table->decimal('amount_preferential', 10, 2);
             $table->foreignId('bank_origin_id')->constrained()->references('id')->on('banks');
             $table->string('bank_origin_account_number');
-            $table->enum('bank_origin_account_type',
-                ['Corriente', 'Ahorro', 'Vista/RUT']
-            );
-            $table->enum('bank_origin_owner_document_type',
-                ['Cédula', 'DNI', 'Pasaporte', 'RUT']
-            );
+            $table->enum('bank_origin_account_type', ['Corriente', 'Ahorro', 'Vista/RUT']);
+            // TODO: quizás estoy hay que estandarizarlo con los mismos tipos que hay en el enum de customer
+            $table->enum('bank_origin_owner_document_type', ['Cédula', 'DNI', 'Pasaporte', 'RUT']);
             $table->string('bank_origin_owner_document_number');
             $table->string('bank_origin_owner_name');
             $table->string('bank_origin_owner_phone');
             $table->string('bank_origin_owner_email');
             $table->json('banks_destinations_ids');
+            // TODO: quizás se necesita otro estado aparte del is_active para poder manejar el (pendiente, procesando, etc)...
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
