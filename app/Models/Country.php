48fbc019dedb78e812f70d4d129af15fbedf8426
Irwing Naranjo <khalisser@gmail.com>
2024-11-04 22:08:06 -0300

    refactor: countries add relation with states

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -12,5 +12,10 @@ class Country extends Model
     protected $table = 'countries';
 
     protected $fillable = ['*'];
+
+    public function states(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(State::class);
+    }
 }
 
