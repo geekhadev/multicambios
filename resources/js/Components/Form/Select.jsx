82d49662e47c1564128e6c42078b7b9783672f0d
Rodolfo Medina <rodolfo.j.medina@gmail.com>
2024-11-13 23:34:54 -0300

    feat: Add 'operator' field to exchanges table migration
    Add a new 'operator' field to the exchanges table migration with a default value of '*'. This field will be used for calculations in the WidgetCalculator component.
    
    refactor: Update Select component in Form
    
    Update the Select component in the Form directory to include an 'infoIcon' prop and an 'iconLabel' prop. This change allows for displaying an information icon next to the label and providing additional information to the user.
    
    refactor: Update WidgetCalculator component
    
    Update the WidgetCalculator component to handle the case when there is only one global exchange. Also, remove the disabled attribute from the select element and the input elements.
    
    feat: Add 'operator' field to ExchangeConfigRates component
    
    Add a new 'operator' field to the ExchangeConfigRates component with a default value of '*'. This field will be used for calculations in the recalculateToReceive and recalculateToSend functions.
    
    refactor: Update Calculator utility functions
    
    Update the Calculator utility functions to include a new 'calculateByOperator' function. This function performs calculations based on the provided operator and operands.

--- resources/js/Components/Form/Select.jsx
+++ resources/js/Components/Form/Select.jsx
@@ -1,8 +1,16 @@
-const Select = ({ id, label, value, onChange, options, required, errors, placeholder }) => {
+const Select = ({ id, label, value, onChange, options, required, errors, placeholder, infoIcon, iconLabel }) => {
   return (
     <div className='px-0.5 mb-2 w-full'>
-      <label htmlFor={id} className="block mb-1 text-sm font-medium text-gray-900">
-        {label} {required && <span className="text-red-500">*</span>}
+      <label htmlFor={id} className="flex flex-nowrap mb-1 text-sm font-medium text-gray-900">
+        {label}
+        {required && <span className="text-red-500 ml-0.5"> * </span>}
+        {infoIcon && (
+          <span className="ml-0.5" title={infoIcon}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
+              <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
+            </svg>
+          </span>
+        )}
       </label>
       <select
         id={id}
