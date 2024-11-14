50a5c0197e14e90ec2b943649edb24dc76326779
Irwing Naranjo <khalisser@gmail.com>
2024-11-14 11:09:04 -0300

    refactor: badge component add default type

--- resources/js/Components/Badge.jsx
+++ resources/js/Components/Badge.jsx
@@ -2,10 +2,11 @@ const TYPES = {
   success: 'bg-green-300 text-green-800',
   danger: 'bg-red-300 text-red-800',
   warning: 'bg-yellow-300 text-yellow-800',
-  info: 'bg-blue-300 text-blue-800'
+  info: 'bg-blue-300 text-blue-800',
+  default: 'bg-gray-300 text-gray-800'
 }
 
-const Badge = ({ children, label, type }) => {
+const Badge = ({ children, label, type = 'default' }) => {
   const baseClasses = 'flex items-center px-2 py-1 text-[8pt] rounded-full h-5 w-auto'
 
   return (
