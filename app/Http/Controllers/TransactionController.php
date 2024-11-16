01d385be51c8da86f1bb08814dd54dfb5c64293c
Irwing Naranjo <khalisser@gmail.com>
2024-11-16 13:15:15 -0300

    feat: add datatable full in transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,15 +6,21 @@ use App\Http\Requests\TransactionRequest;
 use App\Http\Resources\TransactionResource;
 use App\Models\Transaction;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class TransactionController extends Controller
 {
     use AuthorizesRequests;
 
-    public function index()
+    public function index(Request $request)
     {
         $this->authorize('viewAny', Transaction::class);
+
+        $per_page = config('paginate.per_page');
+
+        $sort = $request->input('sort', 'id');
+        $direction = $request->input('direction', 'asc');
 
         $transactions = Transaction::with(
             'exchange',
@@ -22,10 +28,29 @@ class TransactionController extends Controller
             'exchange.destination',
             'confirmed_by',
             'paid_by',
-        )->get();
+        )
+            ->when($request->search, function ($query, $search) {
+                $query->where('id', 'like', '%'.$search.'%')
+                    ->orWhere('created_at', 'like', '%'.$search.'%')
+                    ->orWhere('customer_name', 'like', '%'.$search.'%')
+                    ->orWhere('beneficiary_name', 'like', '%'.$search.'%')
+                    ->orWhere('send_amount', 'like', '%'.$search.'%')
+                    ->orWhere('pay_amount', 'like', '%'.$search.'%')
+                    ->orWhere('send_bank', 'like', '%'.$search.'%')
+                    ->orWhere('receive_bank', 'like', '%'.$search.'%')
+                    ->orWhereHas('confirmed_by', function ($q) use ($search) {
+                        $q->where('name', 'like', '%'.$search.'%');
+                    })
+                    ->orWhereHas('paid_by', function ($q) use ($search) {
+                        $q->where('name', 'like', '%'.$search.'%');
+                    });
+            })
+            ->orderBy($sort, $direction)
+            ->paginate($per_page)
+            ->withQueryString();
 
         return Inertia::render('Transactions/Index', [
-            'transactions' => $transactions,
+            'paginate' => $transactions,
         ]);
     }
 
