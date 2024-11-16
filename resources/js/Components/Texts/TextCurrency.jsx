58251ed2884112fa7503479416899570dced5c8c
Irwing Naranjo <khalisser@gmail.com>
2024-11-16 13:16:21 -0300

    refactor: TextCurrency add className props

--- resources/js/Components/Texts/TextCurrency.jsx
+++ resources/js/Components/Texts/TextCurrency.jsx
@@ -1,8 +1,8 @@
-const TextCurrency = ({ currency = '$', amount }) => {
+const TextCurrency = ({ currency = '$', amount, className }) => {
   // TODO: arreglar el formato de la moneda
   const amount_cleared = amount.includes('.') ? amount.split('.')[0] : amount
   return (
-    <span className='text-xs'>
+    <span className={`text-xs ${className}`}>
       {currency === '$' ? currency + '' : currency + ' '}
       {amount_cleared}
     </span>
