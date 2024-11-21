4a79e4ef4708ffb517cae2f5192d4b2371fa5e26
Rodolfo Medina <rodolfo.j.medina@gmail.com>
2024-11-20 22:17:08 -0300

    refactor: optimize ShareExchanges middleware query

--- app/Http/Middleware/ShareExchanges.php
+++ app/Http/Middleware/ShareExchanges.php
@@ -11,7 +11,13 @@ class ShareExchanges
 {
     public function handle(Request $request, Closure $next)
     {
-        $exchanges = Exchange::with('origin', 'destination', 'bank_origin', 'last_rate')->get();
+        $exchanges = Exchange::whereHas('last_rate', function ($query) {
+            $query->whereBetween('created_at', [
+                date('Y-m-d 00:00:00'),
+                date('Y-m-d 23:59:59')
+            ]);
+        })->with(['origin', 'destination', 'bank_origin', 'last_rate'])->get();
+
         Inertia::share('globalExchanges', $exchanges);
 
         return $next($request);
