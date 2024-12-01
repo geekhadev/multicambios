49546a0407705ef9bdd77e9741c4843f06f9f62d
Irwing Naranjo <khalisser@gmail.com>
2024-12-01 14:36:28 -0300

    feat: add benefits endpoind api

---
+++ app/Http/Controllers/Api/BenefitController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Customer;
+use App\Models\Benefit;
+
+class BenefitController extends Controller
+{
+    public function index()
+    {
+        $customer = Customer::where('user_id', Auth::user()->id)->first();
+
+        if(!$customer) {
+            return response()->json([]);
+        }
+
+        $beneficiaries = Benefit::where('customer_id', $customer->id)
+            ->select(['id', 'name', 'email', 'phone'])
+            ->get();
+
+        return response()->json($beneficiaries);
+    }
+}
